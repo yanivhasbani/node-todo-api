b418a6fffe8eb957c89a9e4b3764ab99cd818af3
Yaniv Hasbani <yaniv@insert.io>
2017-05-31 21:37:00 +0300

    Bug fix on Heroku configuration

--- server/config/config.js
+++ server/config/config.js
@@ -4,21 +4,16 @@
 /**
  * Created by yaniv on 5/29/17.
  */
-var heroku = false;
 if (!process.env.NODE_ENV === 'yaniv') {
-    heroku = true;
+    var env = process.env.TUT_NODE_ENV;
+    if (env === 'development') {
+        process.env.PORT = 4001;
+        process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+    } else if (env === 'test') {
+        process.env.PORT = 4001;
+        //define a different DB in test
+        process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+    }
 }
 
-var env = process.env.TUT_NODE_ENV;
-if (heroku) {
-    env = process.env.NODE_ENV;
-}
 
-if (env === 'development') {
-    process.env.PORT = 4001;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-} else if (env === 'test') {
-    process.env.PORT = 4001;
-    //define a different DB in test
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
-}
