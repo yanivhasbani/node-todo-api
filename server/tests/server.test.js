8d73db7117b04797d6c5e2db7a1ac55c06f45905
Yaniv Hasbani <yaniv@insert.io>
2017-06-02 20:26:59 +0300

    Adding POST /user/login tests

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -258,3 +258,56 @@ describe('POST /users', () => {
             .end(done);
     });
 });
+
+describe('POST /users/login', () => {
+    it('should login user and return auth token', (done) => {
+        request(app)
+            .post('/users/login')
+            .send({
+                email : users[1].email,
+                password : users[1].password,
+            })
+            .expect(200)
+            .expect((res) => {
+                expect(res.headers['x-auth']).toExist();
+            })
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+
+                User.findOne({email : users[1].email}).then((user) => {
+                    expect(user.tokens[0]).toInclude({
+                        access : 'auth',
+                        token: res.headers['x-auth']
+                    });
+                    expect(user.email).toBe(users[1].email);
+                    done();
+                }).catch((e) => done(e));
+            })
+    });
+
+    it('should reject invalid login', (done) => {
+        request(app)
+            .post('/users/login')
+            .send({
+                email : users[1].email,
+                password : users[1].password + '1'
+            })
+            .expect(400)
+            .expect((res) => {
+                expect(res.headers['x-auth']).toNotExist();
+            })
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+
+                User.findOne({email : users[1].email}).then((user) => {
+                    expect(user.tokens.length).toBe(0);
+                    done();
+                }).catch((e) => done(e));
+            })
+        done();
+    });
+});
