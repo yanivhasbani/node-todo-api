3101bedfef5250a0d9cddf901a0d87aba7451f34
Yaniv Hasbani <yaniv@insert.io>
2017-06-03 10:57:49 +0300

    Adding DELETE /users/me/token test, that makes the one before fail

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -311,3 +311,26 @@ describe('POST /users/login', () => {
         done();
     });
 });
+
+describe('DELETE /users/me/token', () => {
+   it('should remove auth token on logout', (done) => {
+       request(app)
+           .delete('/users/me/token')
+           .set({'x-auth' : users[0].tokens[0].token})
+           .expect(200)
+           .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+
+                User.findOne({email : users[0].email}).then((user) => {
+                    if (!user) {
+                        return new Error('No users where found').throw();
+                    }
+                    expect(user.email).toBe(users[0].email);
+                    expect(user.tokens.length).toBe(0);
+                    done();
+                }).catch((e) => done(e));
+           });
+   });
+});
